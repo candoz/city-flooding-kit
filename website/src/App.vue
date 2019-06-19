ec413e154a25991600d903d1c1ba1b2ad5d8a731
Marco Canducci <marco.candoz@gmail.com>
2019-06-19 14:40:25 +0200

    Website prototype with Vuejs

---
+++ website/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <nav-bar></nav-bar>
+    <div>
+      <leaflet-map></leaflet-map>
+    </div>
+  </div>
+</template>
+
+<script>
+import NavBar from "./components/NavBar.vue"
+import LeafletMap from "./components/LeafletMap.vue"
+
+export default {
+  name: 'app',
+  components: {
+    NavBar,
+    LeafletMap
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
