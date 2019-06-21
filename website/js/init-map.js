410fe1a72635d362da3cf9072d474b514ae1f300
Marco Canducci <marco.candoz@gmail.com>
2019-06-21 09:12:37 +0200

    Minor fix

--- website/js/init-map.js
+++ website/js/init-map.js
@@ -1,4 +1,10 @@
-const map = L.map('map',{ center: [44.3239, 10.590], zoom: 8})
+const DEFAULT_LAT = 44.3239
+const DEFAULT_LON = 10.590
+const DEFAULT_ZOOM = 8
+
+const KITS_TABLENAME = "Kits"
+
+const map = L.map('map',{ center: [DEFAULT_LAT, DEFAULT_LON], zoom: DEFAULT_ZOOM})
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', { attribution: ':copyright: OpenStreetMap' }).addTo(map)
 
 fetch("https://54q6hpps8a.execute-api.us-east-2.amazonaws.com/prod/manager", {
@@ -7,37 +13,41 @@ fetch("https://54q6hpps8a.execute-api.us-east-2.amazonaws.com/prod/manager", {
         {
             "operation": "list",
             "payload": {
-                "TableName": "Kits"
+                "TableName": KITS_TABLENAME
             }
         }
     )
-}).then(function(response) {
-console.log("Dentro la response")
-    return response.json();
-  
-}).then(function(responseData){
+
+}).then(response => {
+    console.log("Dentro la response")
+    return response.json()
+
+}).then(responseData => {
+    const markers = []
     responseData.Items.forEach(element => {
         const lat = element.latitude
         const lon = element.longitude
         const kitId = element.kitId
-        const namePopup = kitId.split("-").join(" ").toUpperCase();
-        
-        var marker = L.marker([lat, lon]);
+        const namePopup = kitId.split("-").join(" ").toUpperCase()
+        const marker = L.marker([lat, lon])
 
-        marker.on('click',function (e) {
-            marker.
-            window.location.href = "./sensor-kit.html?kitId="+kitId;
-        });
-        marker.on('mouseover', function (e) {
+        marker.on('click', _ => {
+            marker.window.location.href = "./sensor-kit.html?kitId=" + kitId;
+        })
+        marker.on('mouseover', _ => {
             this.openPopup();
-        });
-        marker.on('mouseout', function (e) {
+        })
+        marker.on('mouseout', _ => {
             this.closePopup();
-        });
+        })
+        const title = "<p align='center'>" + namePopup + "</p> <p align='center'>latitude:" + lat + " longitude:" + lon + "</p>"; 
+        marker.bindPopup(title).addTo(map)
+        markers.push(marker)
+    })
+    return markers
 
-        const title = "<p align='center'>"+namePopup+"</p> <p align='center'>latitude:"+ lat + " longitude:"+ lon+"</p>"; 
-        marker.bindPopup(title).addTo(map);
-    });
-    
+}).then(markers => {
+    const group = new L.featureGroup(markers)
+    map.fitBounds(group.getBounds().pad(0.5))
 
 }).catch(error => console.error(error))
