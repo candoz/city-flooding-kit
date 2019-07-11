126e7b0f3e36ca54cc7107dd7a42ab1c9d1f39f3
DESKTOP-M6HD4PO\lucch <giulia.lucchi7@studio.unibo.it>
2019-07-11 23:14:53 +0200

    Refactoring Website

--- website/js/get-current-values.js
+++ website/js/get-current-values.js
@@ -20,12 +20,18 @@ fetch("https://54q6hpps8a.execute-api.us-east-2.amazonaws.com/prod/manager", {
 }).then(responseData => {
     const element = responseData.Items[0]
     if (element != undefined) {
-        const humidity = "value: "+element.humidity
-        const pressure = "value: "+element.pressure
-        const proximity = "value: "+element.proximity
-        const temperature = "value: "+element.temperature
-        const rain = "value: "+element.raining
-        
+        const humidity = element.humidity+" %"
+        const pressure = element.pressure+" mPa"
+        const proximity = element.proximity+" cm"
+        const temperature = element.temperature+ "°C"
+        var raining=""
+        console.log(element.raining)
+        if(element.raining){
+            raining = "YES"
+        }else{
+            raining= "NO"
+        }
+            
         var time = new Date(element.measureTime*1000).toLocaleString()
         
         document.getElementById("value-pressure").innerHTML = pressure
@@ -33,7 +39,7 @@ fetch("https://54q6hpps8a.execute-api.us-east-2.amazonaws.com/prod/manager", {
         document.getElementById("value-proximity").innerHTML = proximity
         document.getElementById("value-temperature").innerHTML = temperature
         document.getElementById("value-data").innerHTML = time
-        document.getElementById("value-rain").innerHTML = rain
+        document.getElementById("value-raining").innerHTML = raining
     } else {
         document.getElementById("value-pressure").innerHTML = NOT_AVAILABLE_STR
         document.getElementById("value-humidity").innerHTML = NOT_AVAILABLE_STR
