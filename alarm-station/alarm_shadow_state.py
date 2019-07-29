5d8d6a47f2be4279ffd3b5f2ec83a2764ed78361
Marco Canducci <marco.candoz@gmail.com>
2019-07-29 22:00:15 +0200

    Better check with device shadow's delta, plus a fix in JSON payload message string

--- alarm-station/alarm_shadow_state.py
+++ alarm-station/alarm_shadow_state.py
@@ -12,7 +12,7 @@ CERT_FILE = "./certs/e2360f5815-certificate.pem.crt"
 SHADOW_CLIENT = "AlarmStation"
 SHADOW_HANDLER = "AlarmStation"
 TIMEOUT_IN_SECONDS = 2
-SECONDS_TO_SLEEP = 5
+SECONDS_TO_SLEEP = 8
 
 ARDUINO_SERIAL = serial.Serial("/dev/cu.usbmodem1421", 9600)  # "COM3" on Windows
 
@@ -31,18 +31,21 @@ MY_DEVICE_SHADOW = MY_SHADOW_CLIENT.createShadowHandlerWithName(SHADOW_HANDLER, 
 def get_shadow_callback(payload, response_status, token):
 
     state = JSON.loads(str(payload))["state"]
+    # print("State: " + JSON.dumps(state))
+
     desired = state["desired"]
     if desired["alarm"] == "on":
         ARDUINO_SERIAL.write(str.encode("H"))
-        to_update = '{"state":{"reported":' + JSON.dumps(desired) + '}}'
-        MY_DEVICE_SHADOW.shadowUpdate(to_update, update_shadow_callback, TIMEOUT_IN_SECONDS)
+        if state["delta"] is not None:
+            to_update = '{"state":{"reported":' + JSON.dumps(desired) + '}}'
+            MY_DEVICE_SHADOW.shadowUpdate(to_update, update_shadow_callback, TIMEOUT_IN_SECONDS)
 
     data = ARDUINO_SERIAL.read()
     if str(data) == "b'B'":
         now = datetime.datetime.now()
         now_str = now.isoformat()  # Convert to ISO 8601 string
         json_payload = ('{"state":{' +
-                        '"desired":{"alarm":"off", "alarmTime":"' + now_str + '", "alarmReason":""}' +
+                        '"desired":{"alarm":"off", "alarmTime":"' + now_str + '", "alarmReason":""},' +
                         '"reported":{"alarm":"off", "alarmTime":"' + now_str + '", "alarmReason":""}' +
                         '}}')
         MY_DEVICE_SHADOW.shadowUpdate(json_payload, update_shadow_callback, TIMEOUT_IN_SECONDS)
